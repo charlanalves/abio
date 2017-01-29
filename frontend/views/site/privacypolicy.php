b2c6063be1b760f6416c34e789fdc9128a1b7fed
charlanalves <charlan.job@gmail.com>
2017-01-29 20:37:36 -0200

    politica de privacidade

---
+++ frontend/views/site/privacypolicy.php
@@ -0,0 +1,99 @@
+<style>
+   #ppBody
+   {
+   font-size:11pt;
+   width:100%;
+   margin:0 auto;
+   text-align:justify;
+   }
+   #ppHeader
+   {
+   font-family:verdana;
+   font-size:21pt;
+   width:100%;
+   margin:0 auto;
+   }
+   .ppConsistencies
+   {
+   display:none;
+   }
+</style>
+<div id='ppHeader'>App Imprensa Oficial - Política de privacidade</div>
+<div id='ppBody'>
+   <div class='ppConsistencies'>
+      <div class='col-2'>
+         <div class="quick-links text-center">Information Collection</div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links text-center">Information Usage</div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links text-center">Information Protection</div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links text-center">Cookie Usage</div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links text-center">3rd Party Disclosure</div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links text-center">3rd Party Links</div>
+      </div>
+      <div class='col-2'></div>
+      <div class='col-2'></div>
+   </div>
+   <div style='clear:both;height:10px;'></div>
+   <div class='ppConsistencies'>
+      <div class='col-2'>
+         <div class="col-12 quick-links2 gen-text-center">Google AdSense</div>
+      </div>
+      <div class='col-2'>
+         <div class="col-12 quick-links2 gen-text-center">
+            Fair Information Practices
+            <div class="col-8 gen-text-left gen-xs-text-center" style="font-size:12px;position:relative;left:20px;">Fair information<br> Practices</div>
+         </div>
+      </div>
+      <div class='col-2'>
+         <div class="col-12 quick-links2 gen-text-center coppa-pad">
+            COPPA
+         </div>
+      </div>
+      <div class='col-2'>
+         <div class="quick-links2 gen-text-center">Our Contact Information<br></div>
+      </div>
+   </div>
+   <div style='clear:both;height:10px;'></div>
+   <div class='innerText'>
+      Esta política de privacidade foi compilada para melhor servir aqueles que estão preocupados com a forma como as suas "informações de identificação pessoal" está a ser utilizado online.Conforme descrito na lei de privacidade dos EUA e segurança da informação, é informação que pode ser usada por conta própria ou com outras informações para identificar, contatar ou localizar uma única pessoa ou para identificar um indivíduo no contexto. Leia nossa política de privacidade cuidadosamente para obter uma compreensão clara de como coletamos, usamos, protegemos ou manipulamos suas Informações Pessoais de Identificação de acordo com nosso website.
+      <br>
+   </div>
+   <span id='infoCo'></span><br>
+   <div class='grayText'><strong>Que informações pessoais recolhemos das pessoas que visitam o nosso blog, site ou aplicação?</strong></div>
+   <br />
+   <div class='innerText'>Ao efetuar o download do nosso aplicativo, pode ser solitado o acesso ao armazenamento interno de arquivos e o acesso a internet que é fundamental para o funcionamento do app.</div>
+   <br>
+   <div class='grayText'><strong>Quando coletamos informações?</strong></div>
+   <br />
+   <div class='innerText'>O aplicativo acessa essas iformações quando você efetua o download do mesmo.</div>
+   
+   
+   <div class='grayText'><strong>Divulgação de terceiros</strong></div>
+   <br />
+   <div class='innerText'>Não vendemos, comercializamos ou transferimos de outra forma para terceiros suas Informações.</div>
+   <span id='trLi'></span><br>
+   <div class='grayText'><strong>Links de terceiros</strong></div>
+   <br />
+   <div class='innerText'>Não incluímos ou oferecemos produtos ou serviços de terceiros no nosso website.</div>
+   <span id='gooAd'></span><br>
+   <div class='blueText'><strong>Google</strong></div>
+   <br />
+   <div class='innerText'>Não ativamos o Google AdSense em nosso site, mas podemos fazê-lo no futuro.<br><br></div>
+
+
+<div class='blueText'><strong>Contate-nos</strong></div>
+<br />
+<div class='innerText'>Se houver alguma dúvida sobre esta política de privacidade, entre em contato:<br><br></div>
+<div class='innerText'>appimprensa.com.br</div>
+
+
+</div>
