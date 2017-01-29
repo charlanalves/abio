fc7f4cc93d2ee67e43e9ced519733055b3ef1ef7
charlanalves <charlan.job@gmail.com>
2017-01-29 20:41:27 -0200

    politica de privacidade

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -47,6 +47,9 @@ class SiteController extends Controller
     
     public function actionPrivacyPolicy()
     {   
+        ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
       return $this->render('privacypolicy');
     }
     /**
