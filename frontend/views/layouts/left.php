a76feed37ed6aa0c60b19f1a41b3d2041f76f263
Charlan Santos <charlan.job@gmail.com>
2017-11-15 00:18:42 -0200

    adpatacao cod

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -8,7 +8,7 @@
             
             </div>
             <div class="pull-left info">
-                <p><?php //echo Yii::$app->user->identity->username?></p>
+                
 
                 <!--<a href="#"><i class="fa fa-circle text-success"></i> Online</a>-->
             </div>
