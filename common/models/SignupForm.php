c3d8ee647b9b1d89a5069ee8c9b3453cdec40957
Charlan Santos <charlan.job@gmail.com>
2018-04-15 15:29:02 -0300

    revertendo versao do arquivo signupForm

--- common/models/SignupForm.php
+++ common/models/SignupForm.php
@@ -7,8 +7,59 @@ use common\models\User;
 /**
  * Signup form
  */
-class Process extends Model
+class SignupForm extends Model
 {
-   
+    public $username;
+    public $email;
+    public $password;
+    public $User;
+    public $profile;
+    public $id_company;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Já existe um usuário com esse e-mail.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Já existe um usuário com esse e-mail.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+        
+        $this->User = new User();
+        $this->User->username = $this->username;
+        $this->User->id_company = $this->id_company;
+        $this->User->profile = $this->profile;
+        $this->User->email = $this->email;
+        $this->User->setPassword($this->password);
+        $this->User->generateAuthKey();
+        
+        return $this->User->save() ? $this->User : null;
+    }
+  
   
 }
