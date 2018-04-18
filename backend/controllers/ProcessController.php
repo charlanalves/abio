f787b563037e235d2cf3a641c3a641bc04a9ff80
Charlan Santos <charlan.job@gmail.com>
2018-04-17 22:24:56 -0300

    remover acentos ao pesquisar

--- backend/controllers/ProcessController.php
+++ backend/controllers/ProcessController.php
@@ -77,13 +77,16 @@ class ProcessController extends \yii\web\Controller
  
     private function existOccurrence($page, $not)
     {
-        $pos = stripos($page->content, $not->name);
+        $pos = stripos($this->tirarAcentos($page->content), $this->tirarAcentos($not->name));
         if ($pos === false) {
             return false;
         }
         return true;
     }
- 
+    public function tirarAcentos($string){
+       return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
+   }
+
     private function saveOccurrence($page, $not)
     {
         $oc = new Occurrence;        
